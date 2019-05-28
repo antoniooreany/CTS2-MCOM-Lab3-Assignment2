65a08a439368ee60f531e4f6c708228664a0ff40
antoniooreany <antonio_oreany@mail.ru>
2019-05-28 16:34:10 +0200

    redundand code from previous (Lab3) was removed

--- Lab3_Ass2_emulator.s
+++ Lab3_Ass2_emulator.s
@@ -63,21 +63,9 @@ _start:
 	addi	sp, sp, STACK_SIZE	# stack start position should
 					# begin at end of section
 START:
-########################################################################################
-	#mov r7, r0		# COUNTER init
-
-LOOP:
-	#call write_LED		# subroutine write_LED is called
-	#call read_COUNT_BUTTON	# subroutine read_COUNT_BUTTON is called
-	#call read_CLEAR_BUTTON	# subroutine read_CLEAR_BUTTON is called
-	#br LOOP		# check for the key pressed again
-########################################################################################
-
 	movi r15, 0x1068	# r15 <- 4200
 	call init_timer		# subroutine init_timer(r15) is called
-	
 	call wait_timer		# subroutine wait_timer() is called
-	
 	br endloop		# end
 
 init_timer:
@@ -95,50 +83,6 @@ WHILE:
 	andi r15, r15, 0b0001	# mask the content of the status
 	beq r15, r0, WHILE	# if timer is not expired(masked status == 0), check again
 				# the timer has expired(masked status != 0)
-	ret
-
-
-
-########################################################################################
-read_COUNT_BUTTON:
-	PUSH_r9_1
-	PUSH_r9_2
-	movia r9, 0x840		# r9 <- 0x840
-	ldw r9, (r9)		# r9 <- (0x840)
-	andi r9, r9, 0x1	# r9 <- masked value of (0x840)
-	bne r9, r0, RELEASED	# Pressed: if r9!=0 => goto RELEASED
-	br return_read_COUNT_BUTTON
-RELEASED:
-	movia r9, 0x840		# r9 <- 0x840
-	ldw r9, (r9)		# r9 <- (0x840)
-	andi r9, r9, 0x1	# r9 <- masked value of (0x840)
-	bne r9, r0, RELEASED	# Pressed: if r9!=0 => goto RELEASED
-	addi r7, r7, 1		# Pressed: COUNTER++ 
-return_read_COUNT_BUTTON:
-	POP_r9_1
-	POP_r9_2
-	ret	
-	
-read_CLEAR_BUTTON:
-	PUSH_r9_1
-	PUSH_r9_2
-	movia r9, 0x840		# r9 <- 0x840
-	ldw r9, (r9)		# r9 <- (0x840)
-	andi r9, r9, 0x8	# r9 <- masked value of (0x840)
-	beq r9, r0, return_CLEAR_BUTTON	# if r9==0 => goto return_COUNT_BUTTON 
-	mov r7, r0		# COUNTER=0
-return_CLEAR_BUTTON:
-	POP_r9_1
-	POP_r9_2
-	ret			# return
-
-write_LED:
-	PUSH_r9_1
-	PUSH_r9_2
-	movia r9, 0x810		# r9 <- 0x810=output_register_address
-	stw r7, (r9)		# r7 -> (r9) COUNTER -> output_register
-	POP_r9_1
-	POP_r9_2
 	ret
 
 endloop:
